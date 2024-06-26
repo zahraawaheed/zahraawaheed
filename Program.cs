05fe12ca0006e2994045a660d902473ff3d17502
Zahraa Waheed <alzahraa.fathy1700963@science.menofia.edu.eg>
2024-06-26 22:53:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int num = int.Parse(Console.ReadLine());
+            if (num > 0)
+                Console.WriteLine($"{num} is positive");
+            else if (num<0)
+                Console.WriteLine($"{num} is negative");
+            else
+                Console.WriteLine($"{num} is not positive or negative");
+            Console.ReadKey();
+        }
+    }
+}
